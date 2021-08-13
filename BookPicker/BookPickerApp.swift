5a92b680e334a7d8d8cb8c50816fe0cae1a3f1a3
Tzortzis Kapellas <tzortziskap@hotmail.com>
2021-08-13 19:44:51 +0300

    Initial Commit

---
+++ BookPicker/BookPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookPickerApp.swift
+//  BookPicker
+//
+//  Created by Tzortzis Kapellas on 13/8/21.
+//
+
+import SwiftUI
+
+@main
+struct BookPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
